6dba337aef3d7716036234b78853371cfd4c04ff
Prachi <prachikale136@gmail.com>
2023-02-21 16:50:14 +0530

    [Prachi] Add: Single Player at start Position

---
+++ SnakeLadderSimulation/SnakeLadderSimulation/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeLadderSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder Simulation");
+            SinglePlayer singleplayer = new SinglePlayer();
+            singleplayer.StartPlaying();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SnakeLadderSimulation/SnakeLadderSimulation/SinglePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderSimulation
+{
+
+    internal class SinglePlayer
+    {
+        public int StartPoint = 0;
+        public void StartPlaying()
+        {
+            Console.WriteLine("Game started");
+            Console.WriteLine("you are at " +StartPoint);
+        }
+    }
+}
