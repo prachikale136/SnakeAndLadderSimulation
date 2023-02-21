04e8b8251d3490c8ee26091b110b79040362767b
Prachi <prachikale136@gmail.com>
2023-02-22 00:47:10 +0530

    [Prachi] Add: Reaches the winning position till 100

--- SnakeLadderSimulation/SnakeLadderSimulation/SinglePlayer.cs
+++ SnakeLadderSimulation/SnakeLadderSimulation/SinglePlayer.cs
@@ -15,29 +15,36 @@ namespace SnakeLadderSimulation
         public void StartPlaying()
         {
             Console.WriteLine("Game started");
-            Random random = new Random();
-            RandDice = random.Next(1, 7);  //Random value betw 1 to 6
-            Console.WriteLine("Dice Number is : " +RandDice);
-            Choice = random.Next(0, 3); //value to check its snake or ladder
-            Console.WriteLine("Dice value :" +Choice);
-            //Console.WriteLine("you are at " +StartPoint);
+            while (StartPoint != 0)
+            {
+                Random random = new Random();
+                RandDice = random.Next(1, 7);  //Random value betw 1 to 6
+                Console.WriteLine("Dice Number is : " + RandDice);
+                Choice = random.Next(0, 3); //value to check its snake or ladder
+                Console.WriteLine("Dice value :" + Choice);
+                //Console.WriteLine("you are at " +StartPoint);
 
-            switch (Choice)
-            { 
-                case 0:  //for no play if choice =0
-                    Console.WriteLine("No Play");
-                    break;
-                case 1:                         //if choice =1
-                    StartPoint += RandDice;    //startPoint = startPoint +randDice
-                    Console.WriteLine("Got Ladder: " +StartPoint);
-                    break;
-                case 2:
-                    StartPoint -= RandDice;        //if choice=2
-                    Console.WriteLine("snake Attack: " +StartPoint); //startPoint -RandDice
-                    break;
-                default:
-                    Console.WriteLine("Invalid Option");
-                    break;
+                switch (Choice)
+                {
+                    case 0:  //for no play if choice =0
+                        Console.WriteLine("No Play");
+                        break;
+                    case 1:                         //if choice =1
+                        StartPoint += RandDice;    //startPoint = startPoint +randDice
+                        Console.WriteLine("Got Ladder: " + StartPoint);
+                        break;
+                    case 2:
+                        StartPoint -= RandDice;        //if choice=2
+                        Console.WriteLine("snake Attack: " + StartPoint); //startPoint -RandDice
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                if (StartPoint < 0) //if 0<0 then it will start again
+                {
+                    StartPoint= 0;
+                }
             }
         }
     }
